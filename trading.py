bf3c39696f787a0c9f2b0d098c4badefe07c0070
Your Name <you@example.com>
2026-01-14 17:46:44 +0000

    update/create/delete files+folders

--- trading.py
+++ trading.py
@@ -1,6 +1,15 @@
+#import sys
 from sec_edgar_downloader import Downloader
-
 dl = Downloader("YourCompanyName", "your@email.com")
 
-# Download all 8-Ks for a ticker
-dl.get("8-K", "AAPL", after="2022-01-01", before="2025-01-01")
+stock = "msft"
+
+stock = stock.upper()
+print(stock)
+
+print(stock+" getting quarterly reports")
+# Quarterly reports (10-Q)
+dl.get("10-Q", stock, after="2022-01-01", before="2025-01-01")
+print(stock+" getting yearly reports")
+# Annual reports (10-K)
+dl.get("10-K", stock, after="2022-01-01", before="2025-01-01")
