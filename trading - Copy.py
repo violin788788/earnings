cbb85b731ea824b6bfefa991e01a59bd48aa3b15
Your Name <you@example.com>
2026-01-16 14:41:52 -0500

    update/create/delete files+folders

--- trading - Copy.py
+++ trading - Copy.py
@@ -7,8 +7,8 @@ didnt_work=[
 
 
 top_100 = [
-    "C","MS","SO","DUK","BLK",
-    "MET","TSM",
+    "TSM","C","MS","SO","DUK","BLK",
+    "MET",
 
     "NVDA", "AAPL", "GOOGL", "MSFT", "AMZN",
     "AVGO", "META","TSLA",
@@ -34,31 +34,7 @@ top_100 = [
     "MPC", "CVS"
 ]
 
-def earnings_find(folder):
-    files = os.listdir(folder)
-    back = []
-    for folder_name in files:
-        #print(folder_name)
-        file_path = os.path.join(folder, folder_name,"full-submission.txt")
-        with open(file_path, "r", encoding="utf-8") as f:
-            content = f.read()
-        iden = "FILED AS OF DATE:"
-        begin = content.find(iden)
-        end = content.find("\n",begin)
-        earn_date = content[begin:end]
-        #print(earn_date)
-        #print(type(earn_date))
-        #earn33 = earn_date[earn_date.find("\t"):len(earn_date)]
-        #earn33 = earn33.replace("\t","")
-        #print(earn33)
-        #back.append(earn33)
-        back.append(earn_date)
-    return back
 
-
-
-
-    """
 def char_1000(stock):
     stock_file = os.path.join("sec-edgar-filings",stock)
     folder_yearly = os.path.join(stock_file,"10-K")
@@ -88,9 +64,28 @@ def char_1000(stock):
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(content)
         print(f"Truncated quarterly report: {folder_name}")
-        """
 
 
+def earnings_find(folder):
+    files = os.listdir(folder)
+    back = []
+    for folder_name in files:
+        #print(folder_name)
+        file_path = os.path.join(folder, folder_name,"full-submission.txt")
+        with open(file_path, "r", encoding="utf-8") as f:
+            content = f.read()
+        iden = "FILED AS OF DATE:"
+        begin = content.find(iden)
+        end = content.find("\n",begin)
+        earn_date = content[begin:end]
+        #print(earn_date)
+        #print(type(earn_date))
+        #earn33 = earn_date[earn_date.find("\t"):len(earn_date)]
+        #earn33 = earn33.replace("\t","")
+        #print(earn33)
+        #back.append(earn33)
+        back.append(earn_date)
+    return back
 
 def mine_dates(stock):
     stock_file = os.path.join("sec-edgar-filings",stock)
@@ -109,8 +104,8 @@ def mine_dates(stock):
 
 
 
-#import yfinance as yf
-#import pandas as pd
+import yfinance as yf
+import pandas as pd
 
 from pathlib import Path
 import sys,os
@@ -126,7 +121,7 @@ print (cwd)
 #mine_dates(stock)
 #char_1000(stock)
 
-
+#sys.exit()
 #function = sec,mine,gen_report,yahoo
 function = "gen_report"
 #gen..what kind of file to have data?
@@ -134,46 +129,8 @@ stocks = top_100
 output = []
 
 
-#char_1000(stocks)
-def char_1000(stocks):
-    for stock in stocks:
-        stock_file = os.path.join("sec-edgar-filings",stock)
-        times_of_year = os.listdir(stock_file)
-        print(times_of_year)
-        for time_of_year in times_of_year:
-            dir_random = os.path.join(stock_file,time_of_year)
-            randoms = os.listdir(dir_random)
-            for random in randoms:
-                specific_file = os.path.join(dir_random,random,"full-submission.txt")
-                with open(specific_file, "r", encoding="utf-8") as f:
-                    content = f.read()
-                content = content[:1000]  # keep only first 1000 chars
-                with open(specific_file, "w", encoding="utf-8") as f:
-                    f.write(content)
-                print(f"Truncated yearly report: {specific_file}")
-
-
-
-#get_sec_dates(stocks):
-char_1000(stocks)
-#gen_reports(stocks):
-#def get_yahoo(stocks):
-
-
-
-
-char_1000(stocks)
-
-
 
 for a, stock in enumerate(top_100):
-
-
-
-
-
-
-
     print(a, stock)
     if function=="yahoo":
         continue
